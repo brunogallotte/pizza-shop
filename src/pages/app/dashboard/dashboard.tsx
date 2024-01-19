acc934b8ee12396938a62ce56da6631a566986bd
Bruno Gallotte <brunogallotte1@gmail.com>
2024-01-19 19:10:40 -0300

    Feat: add dashboard cards

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,5 +1,10 @@
 import { Helmet } from 'react-helmet-async'
 
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
 export function Dashboard() {
   return (
     <>
@@ -8,7 +13,10 @@ export function Dashboard() {
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
-            
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
         </div>
       </div>
     </>
