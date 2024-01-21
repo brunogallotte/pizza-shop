46037fbe9fe8f1cadebc52c8ecee05f4dab58119
Bruno Gallotte <brunogallotte1@gmail.com>
2024-01-21 11:36:45 -0300

    Feat: add filter order function

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -35,7 +35,7 @@ export function OrderStatus({ status }: OrderStatusProps) {
       {['processing', 'delivering'].includes(status) && (
         <span className="h-2 w-2 rounded-full bg-amber-500" />
       )}
-      
+
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
